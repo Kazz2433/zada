1a4529cb844a019441f97cacbdded7fe27f111da
Kazz2433 <kelvinquida24@gmail.com>
2023-06-25 18:35:37 -0400

    git fix:test for validate checkin after 20 minutes

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,4 +1,4 @@
-import { IUsersRepository } from "@/repositories/Iusers-repository"
+import { IUsersRepository } from "@/repositories/interfaces/Iusers-repository"
 import { hash } from "bcryptjs"
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
 import { User } from "@prisma/client"
--- src/services/validate-checkin.ts
+++ src/services/validate-checkin.ts
@@ -5,6 +5,8 @@ import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 import { MaxDistanceError } from "./errors/max-distance-error";
 import { MaxNumberOfCheckInsError } from "./errors/max-number-of-check-ins-error";
+import dayjs from "dayjs";
+import { LateCheckInValidateError } from "./errors/late-check-in-validate-error";
 
 interface ValidateCheckInServiceRequest{
     checkInId:string
@@ -23,6 +25,15 @@ export class ValidateCheckInService {
         const checkIn = await this.checkInsRepository.findById(checkInId)
 
         if (!checkIn) throw new ResourceNotFoundError()
+
+        const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+            checkIn.created_at,
+            "minutes"
+        )
+
+        if (distanceInMinutesFromCheckInCreation > 20){
+            throw new LateCheckInValidateError()
+        }
 
         checkIn.validate_at = new Date()
 
